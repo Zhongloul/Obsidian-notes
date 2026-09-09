94e4f59c325d58576f0f460b6ed3a304fda4d5f3
Zhongloul <907685071@qq.com>
2026-09-09 20:51:10 +0800

    Add no-cache meta to index

--- .github/generate-index.mjs
+++ .github/generate-index.mjs
@@ -71,6 +71,9 @@ const page = `<!DOCTYPE html>
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="cache-control" content="no-cache, no-store, must-revalidate">
+<meta http-equiv="pragma" content="no-cache">
+<meta http-equiv="expires" content="0">
 <title>我的笔记</title>
 <style>
   * { margin: 0; padding: 0; box-sizing: border-box; }
